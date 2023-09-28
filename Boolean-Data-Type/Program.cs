86dd7cf7b7cc9928828644f4820c5bab37b071a0
Berkay Akbaş <berkay.akbas95@gmail.com>
2023-09-28 17:18:56 +0300

    Object and Boolean Data Types
    Object and Boolean Data Types Notes

---
+++ Boolean-Data-Type/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Data_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool control = true;
+            bool check = 10 > 5;
+            Console.WriteLine(control);
+            Console.WriteLine(check);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Object-Data-Type/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Data_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object berkay = 5;
+            Console.WriteLine(berkay);
+            berkay = "Hello World!!!";
+            Console.WriteLine(berkay);
+            berkay = 3.14;
+            Console.WriteLine( berkay );
+            Console.ReadLine();
+        }
+    }
+}
