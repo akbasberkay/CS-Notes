0be3d488440d9dcafed2e5b660a89710ee7f649a
Berkay2 <Berkay2@DESKTOP-I6UI1RO>
2023-09-28 08:14:22 +0300

    Add project files.

---
+++ Tutorial2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //This is a comment line
+            Console.WriteLine("Hello");
+            Console.Write("1");
+            Console.Write("2");
+            Console.ReadLine();
+
+        }
+    }
+}
