72baa96634f3568cfa1edb84725e85a611a139aa
Berkay Akbaş <berkay.akbas95@gmail.com>
2023-09-28 17:10:18 +0300

    Char and String Data Type
    Tutorial

---
+++ Char-Data-Type/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Char_Data_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ' is important for declaring. Char can only have 1 character.
+            char first = 'A';
+            char second = 'B';
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            // " is used for declaring. Strins can have multiple characters.
+            string third = "Berkay";
+            string fourth = "Akbaş";
+            Console.WriteLine(third);
+            Console.WriteLine(fourth);
+            Console.ReadLine();
+        }
+    }
+}
