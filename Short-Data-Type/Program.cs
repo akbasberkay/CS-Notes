681acdd4590458e60c348007a56dd7e5137c8f61
Berkay Akbaş <berkay.akbas95@gmail.com>
2023-09-28 12:32:05 +0300

    Short Data Type
    Short Data Type Tutorial

---
+++ Short-Data-Type/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Short_Data_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //short daa type can be between the max and min value
+            short maxval = short.MaxValue;
+            short minval = short.MinValue;
+            Console.WriteLine(maxval);
+            Console.WriteLine(minval);
+            Console.ReadLine();
+        }
+    }
+}
