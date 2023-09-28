845d952a7c04c907c159e409112d2aab2ab5b75a
Berkay Akbaş <berkay.akbas95@gmail.com>
2023-09-28 17:28:41 +0300

    Operators

---
+++ Operators/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 365;
+            int b = 5;
+            //Math Operators
+            int c = a + b;
+            Console.WriteLine(c);
+            int d = a - b;
+            Console.WriteLine(d);
+            int e = a * b;
+            Console.WriteLine(e);
+            int f = a / b;
+            Console.WriteLine(f);
+            int g = a % b;
+            Console.WriteLine(g);
+            a-- ;
+            Console.WriteLine(a);
+            a++;
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
