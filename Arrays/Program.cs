55a2c570932be1d2125a7d33d40c5632fbd661d6
Berkay Akbaş <berkay.akbas95@gmail.com>
2023-09-29 14:50:44 +0300

    Update Array Project
    Update Array Project with fun example.

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,18 +10,55 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            string[] cars = { "Porsche", "Lamborghini", "Mercedes", "BMW" };
-            Console.WriteLine(cars);
-            Console.WriteLine(cars[1]);
-            Console.WriteLine(cars.Length);
-            cars = cars.Append("Audi").ToArray();
-            Console.WriteLine(cars.Length);
+            //string[] cars = { "Porsche", "Lamborghini", "Mercedes", "BMW" };
+            //Console.WriteLine(cars);
+            //Console.WriteLine(cars[1]);
+            //Console.WriteLine(cars.Length);
+            //cars = cars.Append("Audi").ToArray();
+            //Console.WriteLine(cars.Length);
 
-            foreach (var car in cars)
+            //foreach (var car in cars)
+            //{
+            //    Console.WriteLine(car);
+            //}
+
+            int[] numbers = new int[10];
+            int evensum = 0;
+            int evencount = 0;
+            int oddsum = 0;
+            int oddcount = 0;
+            for (int i = 0; i < numbers.Length; i++)
             {
-                Console.WriteLine(car);
+                int numtoshow = i + 1;
+                Console.Write(numtoshow + ". number to input: ");
+                int inputnum = Int32.Parse(Console.ReadLine());
+                numbers[i] = inputnum;
             }
 
+            foreach (int j in numbers) { 
+                if (j % 2 == 0) {
+                    evencount++;
+                    evensum += j;
+                }
+                else
+                {
+                    oddsum += j;
+                    oddcount++;
+                }
+            }
+            Console.WriteLine("Odd number count: " + oddcount);
+            Console.WriteLine("Even number count: " + evencount);
+            Console.WriteLine(evensum);
+            Console.WriteLine(oddsum);
+            if (evensum > oddsum) {
+                Console.WriteLine("Even numbers are bigger than odd numbers!");
+            } else if (oddsum > evensum) {
+                Console.WriteLine("Odd numbers are bigger than even numbers!");
+            }
+            else
+            {
+                Console.WriteLine("Odd numbers are equal to even numbers!");
+            }
             Console.ReadLine();
         }
     }
