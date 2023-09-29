5ec544b3a24dfbb683d895d6d9fe7639dab8e4de
Berkay Akbaş <berkay.akbas95@gmail.com>
2023-09-29 08:27:33 +0300

    For Loops
    For Loops Example

---
+++ For-Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the printed amount:");
+            string amnt = Console.ReadLine();
+            int amntint = Convert.ToInt32(amnt);
+            for (int i = 0; i < amntint; i++) { 
+                Console.WriteLine(i);
+                Console.WriteLine("Have a good day!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
