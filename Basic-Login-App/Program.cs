6db1808f3d673bc902e20da9edb198c066c35c3b
Berkay Akbaş <berkay.akbas95@gmail.com>
2023-09-29 12:58:47 +0300

    While Loops and Basic Login App
    While Loops and Basic Login App

---
+++ Basic-Login-App/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Login_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "berkay";
+            string password = "akbas";
+            string usrinput = "";
+            string passinput = "";
+            int errcount = 0;
+            while (true)
+            {
+                Console.WriteLine("Please type in username:");
+                usrinput = Console.ReadLine();
+                Console.WriteLine("Please type in password:");
+                passinput = Console.ReadLine();
+                if (usrinput != username || passinput != password)
+                {
+                    errcount ++;
+                    Console.WriteLine("Incorrect attempt! " + (3-errcount) + " attempts left!");
+                    if (errcount == 3)
+                    {
+                        Console.WriteLine("Login failed!");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Welcome!");
+                    break;
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ While-Loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (x < 10)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+            Console.WriteLine("---------------");
+            int cnt = 100;
+            while (cnt >= 0) { 
+                if (cnt % 2 == 0)
+                {
+                    Console.WriteLine(cnt);
+                }
+                cnt--;
+            }
+            Console.ReadLine();
+        }
+    }
+}
