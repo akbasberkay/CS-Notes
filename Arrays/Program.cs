1ba294b9a6289ea40eaea4d7c8f21dd6b0e7fe5a
Berkay Akbaş <berkay.akbas95@gmail.com>
2023-09-29 14:31:50 +0300

    Array Knowledge
    Array Knowledge

---
+++ Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "Porsche", "Lamborghini", "Mercedes", "BMW" };
+            Console.WriteLine(cars);
+            Console.WriteLine(cars[1]);
+            Console.WriteLine(cars.Length);
+            cars = cars.Append("Audi").ToArray();
+            Console.WriteLine(cars.Length);
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
