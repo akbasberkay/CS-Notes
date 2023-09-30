a42b0f49d505638b9b3098732fcc05e72b5f024b
Berkay Akbaş <berkay.akbas95@gmail.com>
2023-09-30 12:41:56 +0300

    Getter Setter
    Simple Getter Setter Example

---
+++ Getter-Setter/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getter_Setter
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public void setName(string _name)
+        {
+            name = _name;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public void setAge(int _age)
+        {
+            age = _age;
+        }
+        public int getAge()
+        {
+            return age;
+        }
+    }
+}
---
+++ Getter-Setter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getter_Setter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person UncleJeff = new Person();
+            UncleJeff.setName("Elon Musk");
+            UncleJeff.setAge(51);
+            Console.WriteLine("Name: " +  UncleJeff.getName());
+            Console.WriteLine("Age: " + UncleJeff.getAge());
+            Console.ReadLine();
+        }
+    }
+}
