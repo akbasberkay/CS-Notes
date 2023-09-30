92f5b4d83441009f06f02d8e854f0701072e49ba
Berkay Akbaş <berkay.akbas95@gmail.com>
2023-09-30 11:46:38 +0300

    Public and Private Class Difference
    Public and Private Class Difference

---
+++ Public-and-Private-Classes/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Public_and_Private_Classes
+{
+    public class Customer
+    {
+        public int id;
+        public string custname;
+        public string custsurname;
+        private string gender;
+        public Customer(int _id, string _custname, string _custsurname, string _gender)
+        {
+            Console.WriteLine("Constructing...");
+            id = _id;
+            custname = _custname;
+            custsurname = _custsurname;
+            gender = _gender;
+        }
+
+        public void SayHi()
+        {
+            Console.WriteLine("Hello " + custname + " " + custsurname);
+            SayLo("aaa","bbb");
+        }
+
+        private void SayLo(string frst, string scnd) {
+            Console.WriteLine("Hello " + frst + " " + scnd);
+        }
+    }
+}
---
+++ Public-and-Private-Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Public_and_Private_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer obj = new Customer(_id:1, _custname:"Berkay",_custsurname:"Akbas",_gender:"Male");
+            
+            //obj.id = 1;
+            //obj.custname = "Berkay";
+            //obj.custsurname = "Akbas";
+            // obj.gender = "Male"; Inaccessible due to private 
+
+            obj.SayHi();
+            //obj.SayLo(); Inaccessible due to private
+
+            // private methods can be accessed inside of other class methods of the same class
+            // same goes for private attributes as well
+            Console.ReadLine();
+    }
+    }
+}
