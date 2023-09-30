0faa69f4e80045cd3bc891ce482af7ac09099af5
Berkay Akbaş <berkay.akbas95@gmail.com>
2023-09-30 13:01:22 +0300

    Properties
    Simple Properties get&set example.

---
+++ Property/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public string NAME { 
+            get {
+                return name;
+            } set { 
+                name = value;
+            }
+        
+        }
+        public int AGE { 
+            get {
+                return age;
+            } 
+            set { 
+                age = value;
+            } 
+        }
+    }
+}
---
+++ Property/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person UncleJeff = new Person();
+            UncleJeff.NAME = "Elon Musk";
+            UncleJeff.AGE = 51;
+
+            Console.WriteLine("Name: " + UncleJeff.NAME);
+            Console.WriteLine("Age: " + UncleJeff.AGE);
+            Console.ReadLine();
+        }
+    }
+}
