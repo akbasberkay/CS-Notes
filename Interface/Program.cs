e0bb8e159d4d63095f68c4c040819f6fac22d600
Berkay Akbaş <berkay.akbas95@gmail.com>
2023-10-02 15:15:23 +0300

    Update Program.cs

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -13,7 +13,23 @@ namespace Interface
             Hyena hyena = new Hyena();
             hyena.Hunt();
             hyena.Flee();
+            Person berkay = new Person("Berkay", 25);
+            berkay.sayHi();
             Console.ReadLine();
+        }
+        public class Person
+        {
+            private string name;
+            private int age;
+            public Person(string _name, int _age)
+            {
+                name = _name;
+                age = _age;
+            }
+            public void sayHi()
+            {
+                Console.WriteLine("Hello " + name + " with age " + age);
+            }
         }
     }
 }
